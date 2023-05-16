fd060f0c264b770b3c753857cc8c220678f75789
jichen <jichen.guo@dfki.de>
2023-05-16 22:45:48 +0200

    Separate training and test part in one script & record results

--- src/maml_train_test.py
+++ src/maml_train_test.py
@@ -104,6 +104,15 @@ elif args.order == True:
     profiler = Profiler('FO-MAML_{}_{}-way_{}-shot_{}-queries'.format(
         args.dataset, args.n_ways, args.k_shots, args.q_shots), args.experiment, args)
 
+save_dir = os.path.join(os.getcwd(), 'MAML_results')
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+experiment_name = 'MAML_{}_{}-way_{}-shot_{}-queries_iter_{}'.format(args.dataset,args.n_ways, args.k_shots, args.q_shots, args.iterations)
+res_dir = os.path.join(save_dir, experiment_name)
+if not os.path.exists(res_dir):
+    os.makedirs(res_dir)
+print('results directory: {}'.format(res_dir))
 
 def accuracy(predictions, targets):
     predictions = predictions.argmax(dim=1).view(targets.shape)
@@ -116,8 +125,14 @@ def fast_adapt(batch, learner, loss, adaptation_steps, shots, ways, device):
     print(np.shape(data))
     # Separate data into adaptation/evalutation sets
     adaptation_indices = th.zeros(data.size(0)).byte()
-    # select the elements based on number of shots and ways, then seperate the training and validation here
-    adaptation_indices[th.arange(shots*ways)] = 1  # original th.arange(shots*ways)*2
+    # original: select the elements based on number of shots and ways, then seperate the training and validation here
+    # now just separate in 80% train - 20 % val
+    train_indices = random.sample(range(data.size(0)), int(data.size(0)*0.8))
+    adaptation_indices[train_indices] = 1
+
+    #adaptation_indices[th.arange(int(data.size(0)*0.8))] = 1
+    #adaptation_indices[np.arange(shots*ways) * 2] = True
+
     adaptation_data, adaptation_labels = data[adaptation_indices], labels[adaptation_indices]
     evaluation_data, evaluation_labels = data[1 - adaptation_indices], labels[1 - adaptation_indices]
 
@@ -161,14 +176,23 @@ def main(
     opt = optim.Adam(maml.parameters(), meta_lr)
     loss = nn.CrossEntropyLoss(reduction='mean')
 
+    results_dict = {}
+    results_dict['train_err'] = {}
+    results_dict['train_acc'] = {}
+    results_dict['valid_err'] = {}
+    results_dict['valid_acc'] = {}
+    results_dict['test_err'] = {}
+    results_dict['test_acc'] = {}
+
     for iteration in range(num_iterations):
+
         opt.zero_grad()
         meta_train_error = 0.0
         meta_train_accuracy = 0.0
         meta_valid_error = 0.0
         meta_valid_accuracy = 0.0
-        meta_test_error = 0.0
-        meta_test_accuracy = 0.0
+        # meta_test_error = 0.0
+        # meta_test_accuracy = 0.0
         for task in range(meta_batch_size):
             # Compute meta-training loss
             learner = maml.clone()
@@ -197,34 +221,68 @@ def main(
             meta_valid_error += evaluation_error.item()
             meta_valid_accuracy += evaluation_accuracy.item()
 
-            # Compute meta-testing loss
-            learner = maml.clone()
-            batch = test_tasks.sample()
-            evaluation_error, evaluation_accuracy = fast_adapt(batch,
-                                                               learner,
-                                                               loss,
-                                                               adaptation_steps,
-                                                               shots,
-                                                               ways,
-                                                               device)
-            meta_test_error += evaluation_error.item()
-            meta_test_accuracy += evaluation_accuracy.item()
+            # # Compute meta-testing loss
+            # learner = maml.clone()
+            # batch = test_tasks.sample()
+            # evaluation_error, evaluation_accuracy = fast_adapt(batch,
+            #                                                    learner,
+            #                                                    loss,
+            #                                                    adaptation_steps,
+            #                                                    shots,
+            #                                                    ways,
+            #                                                    device)
+            # meta_test_error += evaluation_error.item()
+            # meta_test_accuracy += evaluation_accuracy.item()
 
         # Print some metrics
+        train_err = meta_train_error / meta_batch_size
+        train_acc =  meta_train_accuracy / meta_batch_size
+        valid_err = meta_valid_error / meta_batch_size
+        valid_acc = meta_valid_accuracy / meta_batch_size
         print('\n')
         print('Iteration', iteration)
-        print('Meta Train Error', meta_train_error / meta_batch_size)
-        print('Meta Train Accuracy', meta_train_accuracy / meta_batch_size)
-        print('Meta Valid Error', meta_valid_error / meta_batch_size)
-        print('Meta Valid Accuracy', meta_valid_accuracy / meta_batch_size)
-        print('Meta Test Error', meta_test_error / meta_batch_size)
-        print('Meta Test Accuracy', meta_test_accuracy / meta_batch_size)
+        print('Meta Train Error', train_err)
+        print('Meta Train Accuracy', train_acc)
+        print('Meta Valid Error', valid_err)
+        print('Meta Valid Accuracy', valid_acc)
+        # print('Meta Test Error', meta_test_error / meta_batch_size)
+        # print('Meta Test Accuracy', meta_test_accuracy / meta_batch_size)
 
         # Average the accumulated gradients and optimize
         for p in maml.parameters():
             p.grad.data.mul_(1.0 / meta_batch_size)
         opt.step()
 
+        results_dict["train_err"][iteration] = train_err
+        results_dict["train_acc"][iteration] = train_acc
+        results_dict["valid_err"][iteration] = valid_err
+        results_dict["valid_acc"][iteration] = valid_acc
+
+    meta_test_error = 0.0
+    meta_test_accuracy = 0.0
+    for task in range(meta_batch_size):
+        # Compute meta-testing loss
+        learner = maml.clone()
+        batch = test_tasks.sample()
+        evaluation_error, evaluation_accuracy = fast_adapt(batch,
+                                                           learner,
+                                                           loss,
+                                                           adaptation_steps,
+                                                           shots,
+                                                           ways,
+                                                           device)
+        meta_test_error += evaluation_error.item()
+        meta_test_accuracy += evaluation_accuracy.item()
+    test_err = meta_test_error / meta_batch_size
+    test_acc = meta_test_accuracy / meta_batch_size
+
+    print('Meta Test Error', test_err)
+    print('Meta Test Accuracy', test_acc)
+    results_dict["test_err"] = test_err
+    results_dict["test_acc"] = test_acc
+
+    with open(os.path.join(res_dir, 'results.json'), "w") as json_file:
+        json.dump(results_dict, json_file, indent=4)
 
 if __name__ == '__main__':
     main() 
